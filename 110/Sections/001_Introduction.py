acf57993127b702712bf489aad2919fa8c4245c5
cs101academy <119630731+cs101academy@users.noreply.github.com>
2023-04-10 12:10:47 -0500

    Update 001_Introduction.py

--- 110/Sections/001_Introduction.py
+++ 110/Sections/001_Introduction.py
@@ -1,10 +1,8 @@
 # © CS101 Academy - www.cs101.io
-# Content Name : Programming Fundamentals CS101 
-# Last_Update  : 10 April 2023 By: YA
-# Links  : https://www.cs101.io/contents/cs101-Programming-Fundamentals
-#############################################
-
-# Programming Fundamentals CS101 - Book and Videos
+# Programming Fundamentals CS101 - 001_Introduction.py
+# Link: https://www.cs101.io/contents/110/Programming_Fundamentals_CS101/
+# Last_Update: 10 April 2023 - By: YA
+#########################################################################
 
 
 
@@ -12,5 +10,4 @@
 
 
 
-
-###### CS101.io - End #######################
+###### CS101.io - End ###################################################
